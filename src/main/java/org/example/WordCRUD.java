a28af0c22e887fcf8ef5e2e4d5698800702e6131
seol-wonjun <qiops@naver.com>
2023-09-15 10:54:04 +0900

    최종 코드

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -140,7 +140,7 @@ public class WordCRUD implements ICRUD{
                 count ++;
             }
             br.close();
-            System.out.println("==> " + count + "개 로딩 완료!!!");
+            System.out.println("==> " + count + "개 단어 로딩 완료!!!");
         } catch (IOException e) {
             e.printStackTrace();
         }
